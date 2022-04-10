bf7872f4f7f43e70e747647504586db0d21e6c89
silvinogustavo <silvinogustavo@gmail.com>
2022-04-10 10:03:14 -0300

    4 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,10 @@
-import { Component, VERSION } from '@angular/core';
-import { MatIconRegistry } from '@angular/material/icon';
-import { DomSanitizer } from '@angular/platform-browser';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./gama-presentation.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
-  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
-    matIconRegistry.addSvgIcon(
-      'angular',
-      domSanitizer.bypassSecurityTrustResourceUrl('https://0t2.github.io/angular-material-notes/svg/angular.svg'))
-      .addSvgIconInNamespace(
-      'custom-svg',
-      'angular',
-      domSanitizer.bypassSecurityTrustResourceUrl('https://0t2.github.io/angular-material-notes/svg/angular_solidBlack.svg'))
-      .addSvgIconSetInNamespace('core',
-      domSanitizer.bypassSecurityTrustResourceUrl('https://0t2.github.io/angular-material-notes/svg/core-icon-set.svg'))
-      .registerFontClassAlias('fontawesome', 'fa');
-  }
+export class AppComponent {
+  constructor() {}
 }

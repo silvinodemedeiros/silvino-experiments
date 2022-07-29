b5634772df44c79244108e7261480051e7e3343c
silvinodemedeiros <silvinogustavo@gmail.com>
2022-07-29 07:21:06 -0300

    implements backwards progress spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,13 +27,15 @@ export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
 
   spinnerValue$ = new BehaviorSubject(0);
   mouseHold$;
+  decreaseRate = 2;
 
   sub = new Subscription();
 
   todo = [
     'Componentize button',
     'Customize size',
-    'Parametrize attributes'
+    'Parametrize attributes',
+    'Implement event for when timer reaches 100%',
   ];
 
   constructor() {}
@@ -43,14 +45,22 @@ export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
   ngAfterViewInit() {
     const button = this.timedButton.nativeElement;
     const mouseDown$ = fromEvent<MouseEvent>(button, 'mousedown');
-    const deactivate$ = fromEvent(button, 'mouseleave').pipe(
+    const mouseEnd$ = fromEvent(button, 'mouseleave').pipe(
       mergeWith(fromEvent(button, 'mouseup'))
     );
 
     this.mouseHold$ = mouseDown$.pipe(
-      switchMap(() => interval(10).pipe(
-        takeUntil(deactivate$)
-      ))
+      switchMap(() => interval(10).pipe(takeUntil(mouseEnd$)))
+    );
+
+    this.sub.add(
+      mouseEnd$.subscribe((val: number) => {
+        if (val - this.decreaseRate < this.decreaseRate) {
+          this.spinnerValue$.next((val - this.decreaseRate));
+        } else {
+          this.spinnerValue$.next(0);
+        }
+      })
     );
 
     this.sub.add(

0bbb49532ffcbf63dc68ebe5fcec8e0e062e2877
silvinodemedeiros <silvinogustavo@gmail.com>
2022-09-04 18:18:35 -0300

    fixes transition to indeterminate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,12 @@
 import { trigger } from '@angular/animations';
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  OnDestroy,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
-import { BehaviorSubject, fromEvent, Subscription, interval } from 'rxjs';
-import { mergeWith, switchMap, takeUntil } from 'rxjs';
-import { map } from 'rxjs';
-import { takeWhile, debounceTime } from 'rxjs';
+import { Component, OnInit, } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-component',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  animations: [
-    trigger('fadeSpinIn', [])
-  ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
-  @ViewChild('timedButton', { read: ElementRef }) timedButton;
-
-  spinnerValue = 0;
-  spinnerValue$ = new BehaviorSubject(0);
-  intervalRate = 10;
-  progressIncrement = 1;
-
-  sub = new Subscription();
+export class AppComponent implements OnInit {
 
   todo = [
     'Customize size',
@@ -36,57 +14,7 @@ export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
     'Implement event for when timer reaches 100%',
   ];
 
-  set progress(value) {
-    this.spinnerValue = value;
-    this.spinnerValue$.next(value);
-  }
-
   constructor() {}
 
   ngOnInit() {}
-
-  ngAfterViewInit() {
-    const button = this.timedButton.nativeElement;
-
-    const mouseEnd$ = fromEvent(button, 'mouseleave').pipe(
-      mergeWith(fromEvent(button, 'mouseup')),
-      switchMap(() =>
-        interval(this.intervalRate).pipe(
-          map(() => {
-            return this.spinnerValue - this.progressIncrement >= 0
-              ? (this.spinnerValue -= this.progressIncrement)
-              : (this.spinnerValue = 0);
-          }),
-          takeUntil(mouseHold$)
-        )
-      )
-    );
-
-    const mouseHold$ = fromEvent<MouseEvent>(button, 'mousedown').pipe(
-      switchMap(() =>
-        interval(this.intervalRate).pipe(
-          map(() =>
-            this.spinnerValue + this.progressIncrement <= 100
-              ? (this.spinnerValue += this.progressIncrement)
-              : (this.spinnerValue = 100)
-          ),
-          takeUntil(mouseEnd$)
-        )
-      )
-    );
-
-    const sub = mouseHold$.pipe(mergeWith(mouseEnd$)).subscribe(
-      (val) => {
-        if (val >= 0 && val <= 100) {
-          this.progress = val;
-        }
-      }
-    );
-
-    this.sub.add(sub);
-  }
-
-  ngOnDestroy() {
-    this.sub.unsubscribe();
-  }
 }
--- src/app/components/timed-button/timed-button.component.ts
+++ src/app/components/timed-button/timed-button.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { BehaviorSubject, fromEvent, mergeWith, Subscription, switchMap, interval, map, takeUntil } from 'rxjs';
+import { BehaviorSubject, fromEvent, mergeWith, Subscription, switchMap, interval, map, takeUntil, debounceTime, timer } from 'rxjs';
 
 @Component({
   selector: 'app-timed-button',
@@ -10,13 +10,15 @@ export class TimedButtonComponent implements OnInit, OnDestroy, AfterViewInit {
 
   @ViewChild('timedButton', { read: ElementRef }) timedButton;
 
-  @Input() intervalRate = 20;
+  @Input() intervalRate = 10;
 
   spinnerValue = 0;
   spinnerValue$ = new BehaviorSubject(0);
   progressIncrement = 1;
 
   sub = new Subscription();
+
+  progressComplete$ = new BehaviorSubject(false);
 
   set progress(value) {
     this.spinnerValue = value;
@@ -34,11 +36,11 @@ export class TimedButtonComponent implements OnInit, OnDestroy, AfterViewInit {
       mergeWith(fromEvent(button, 'mouseup')),
       switchMap(() =>
         interval(this.intervalRate).pipe(
-          map(() => {
-            return this.spinnerValue - this.progressIncrement >= 0
+          map(() =>
+            this.spinnerValue - this.progressIncrement >= 0
               ? (this.spinnerValue -= this.progressIncrement)
-              : (this.spinnerValue = 0);
-          }),
+              : (this.spinnerValue = 0)
+          ),
           takeUntil(mouseHold$)
         )
       )
@@ -48,19 +50,21 @@ export class TimedButtonComponent implements OnInit, OnDestroy, AfterViewInit {
       switchMap(() =>
         interval(this.intervalRate).pipe(
           map(() =>
-            this.spinnerValue + this.progressIncrement <= 100
+            this.spinnerValue + this.progressIncrement <= 130
               ? (this.spinnerValue += this.progressIncrement)
-              : (this.spinnerValue = 100)
+              : (this.spinnerValue = 130)
           ),
           takeUntil(mouseEnd$)
         )
       )
     );
 
-    const sub = mouseHold$.pipe(mergeWith(mouseEnd$)).subscribe(
+    const sub = mouseHold$.pipe(mergeWith(mouseEnd$)).pipe().subscribe(
       (val) => {
-        if (val >= 0 && val <= 100) {
+        if (val >= 0 && val < 130) {
           this.progress = val;
+        } else if (val === 130) {
+          this.progressComplete$.next(true)
         }
       }
     );

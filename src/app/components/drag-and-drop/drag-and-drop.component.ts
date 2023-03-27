175883021e46f7b2f8e72146d93dbfc756663d6a
silvinodemedeiros <silvinogustavo@gmail.com>
2023-03-27 08:56:37 -0300

    implements basic drag and drop

--- src/app/components/drag-and-drop/drag-and-drop.component.ts
+++ src/app/components/drag-and-drop/drag-and-drop.component.ts
@@ -7,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DragAndDropComponent implements OnInit {
 
+  widgetList = [
+    {name: 'A'},
+    {name: 'B'},
+    {name: 'C'},
+    {name: 'D'},
+    {name: 'E'}
+  ];
+
+  allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
+
   constructor() { }
 
   ngOnInit() {
